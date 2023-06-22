2f0deb7697d611eeb04e3a930bb74c6c49d34a2f
Tiren74n <40044281+7ansyah@users.noreply.github.com>
2023-06-22 22:19:15 +0700

    Add files via upload

---
+++ juguagan.js
@@ -0,0 +1,99 @@
+function verifikasiPassword() {
+var password = prompt("Masukkan password:"); var passwordBenar = "juguagan";
+if (password === passwordBenar) {
+document.getElementById("konten").style.display = "block"; 
+} else {
+alert("Password salah. Halaman akan ditutup."); window.close(); } }
+
+function verifikasiPassword() {
+  var password = prompt("Masukkan password:");
+  var passwordBenar = "juguagan";
+  
+  if (password === passwordBenar) {
+    document.getElementById("konten").style.display = "block";
+  } else {
+    alert("Password salah. Mengalihkan ke halaman daftar 55five");
+    window.location.href = "http://170.33.96.215:55/#/register?r_code=ClEgG1195363";
+  }
+}
+    var data = [
+      { sebelumnya: "KKKKK", prediksi_pasangan: "Bx1 Kx2 Kx5 Kx11 Bx23 Bx49 Bx102 Kx210 Kx437", catatan: "pasangan terjauh biasanya di Kx210" },
+      { sebelumnya: "BBBBB", prediksi_pasangan: "Kx1 Bx2 Bx5 Bx11 Kx23 Kx49 Kx102 Bx210 Bx437", catatan: "pasangan terjauh biasanya di Bx210" },
+      { sebelumnya: "KKKKB", prediksi_pasangan: "Kx1 Kx2 Bx5 Bx11 Bx23 Kx49 Kx102 Kx210 Bx437", catatan: "pasangan terjauh biasanya di Bx437" },
+      { sebelumnya: "BBBBK", prediksi_pasangan: "Bx1 Bx2 Kx5 Kx11 Kx23 Bx49 Bx102 Bx210 Kx437", catatan: "pasangan terjauh biasanya di kx437" },
+      { sebelumnya: "KKKBB", prediksi_pasangan: "Kx1 Bx2 Bx5 Bx11 Kx23 Kx49 Kx102 Bx210 Bx437", catatan: "pasangan terjauh biasanya di Kx49" },
+      { sebelumnya: "BBBKK", prediksi_pasangan: "Bx1 Kx2 Kx5 Kx11 Bx23 Bx49 Bx102 Kx210 Kx437", catatan: "pasangan terjauh biasanya di Bx49" },
+      { sebelumnya: "KKKBK", prediksi_pasangan: "Bx1 Bx2 Kx5 Kx11 Kx23 Bx49 Bx102 Bx210 Kx437", catatan: "pasangan terjauh biasanya di Bx210" },
+      { sebelumnya: "BBBKB", prediksi_pasangan: "Kx1 Kx2 Bx5 Bx11 Bx23 Kx49 Kx102 Kx210 Bx437", catatan: "pasangan terjauh biasanya di Kx210" },
+      { sebelumnya: "KKBKK", prediksi_pasangan: "Bx1 Kx2 Kx5 Kx11 Bx23 Bx49 Bx102 Kx210 Kx437", catatan: "pasangan terjauh biasanya di Bx102" },
+      { sebelumnya: "BBKBB", prediksi_pasangan: "Kx1 Bx2 Bx5 Bx11 Kx23 Kx49 Kx102 Bx210 Bx437", catatan: "pasangan terjauh biasanya di Kx102" },
+      { sebelumnya: "KKBBK", prediksi_pasangan: "Bx1 Bx2 Kx5 Kx11 Kx23 Bx49 Bx102 Bx210 Kx437", catatan: "pasangan terjauh biasanya di Bx102" },
+      { sebelumnya: "BBKKB", prediksi_pasangan: "Kx1 Kx2 Bx5 Bx11 Bx23 Kx49 Kx102 Kx210 Bx437", catatan: "pasangan terjauh biasanya di Kx102" },
+      { sebelumnya: "KKBBB", prediksi_pasangan: "Bx1 Bx2 Bx5 Kx11 Kx23 Kx49 Bx102 Bx210 Bx437", catatan: "pasangan terjauh biasanya di Kx49" },
+      { sebelumnya: "BBKKK", prediksi_pasangan: "Kx1 Kx2 Kx5 Bx11 Bx23 Bx49 Kx102 Kx210 Kx437", catatan: "pasangan terjauh biasanya di Kx49" },
+      { sebelumnya: "KKBKB", prediksi_pasangan: "Kx1 Kx2 Bx5 Bx11 Bx23 Kx49 Kx102 Kx210 Bx437", catatan: "pasangan terjauh biasanya di Kx210" },
+      { sebelumnya: "BBKBK", prediksi_pasangan: "Bx1 Bx2 Kx5 Kx11 Kx23 Bx49 Bx102 Bx210 Kx437", catatan: "pasangan terjauh biasanya di Bx210" },
+      { sebelumnya: "KBKKK", prediksi_pasangan: "Bx1 Kx2 Kx5 Kx11 Bx23 Bx49 Bx102 Kx210 Kx437", catatan: "pasangan terjauh biasanya di Kx210" },
+      { sebelumnya: "BKBBB", prediksi_pasangan: "Kx1 Bx2 Bx5 Bx11 Kx23 Kx49 Kx102 Bx210 Bx437", catatan: "pasangan terjauh biasanya di Kx210" },
+      { sebelumnya: "KBBKK", prediksi_pasangan: "Bx1 Kx2 Kx5 Kx11 Bx23 Bx49 Bx102 Kx210 Kx437", catatan: "pasangan terjauh biasanya di Bx314" },
+      { sebelumnya: "BKKBB", prediksi_pasangan: "Kx1 Bx2 Bx5 Bx11 Kx23 Kx49 Kx102 Bx210 Bx437", catatan: "pasangan terjauh biasanya di Kx102" },
+      { sebelumnya: "KBBBK", prediksi_pasangan: "Bx1 Kx2 Kx5 Kx11 Bx23 Bx49 Bx102 Kx210 Kx437", catatan: "pasangan terjauh biasanya di Kx437" },
+      { sebelumnya: "BKKKB", prediksi_pasangan: "Kx1 Bx2 Bx5 Bx11 Kx23 Kx49 Kx102 Bx210 Bx437", catatan: "pasangan terjauh biasanya di Bx437" },
+      { sebelumnya: "KBBBB", prediksi_pasangan: "Kx1 Bx2 Bx5 Bx11 Kx23 Kx49 Kx102 Bx210 Bx437", catatan: "pasangan terjauh biasanya di Bx210" },
+      { sebelumnya: "BKKKK", prediksi_pasangan: "Bx1 Kx2 Kx5 Kx11 Bx23 Bx49 Bx102 Kx210 Kx437", catatan: "pasangan terjauh biasanya di Kx210" },
+      { sebelumnya: "KBKBB", prediksi_pasangan: "Kx1 Bx2 Bx5 Bx11 Kx23 Kx49 Kx102 Bx210 Bx437", catatan: "pasangan terjauh biasanya di Bx210" },
+      { sebelumnya: "BKBKK", prediksi_pasangan: "Bx1 Kx2 Kx5 Kx11 Bx23 Bx49 Bx102 Kx210 Kx437", catatan: "pasangan terjauh biasanya di Kx210" },
+      { sebelumnya: "KBKBK", prediksi_pasangan: "Bx1 Bx2 Kx5 Kx11 Kx23 Bx49 Bx102 Bx210 Kx437", catatan: "pasangan terjauh biasanya di Bx210" },
+      { sebelumnya: "BKBKB", prediksi_pasangan: "Kx1 Kx2 Bx5 Bx11 Bx23 Kx49 Kx102 Kx210 Bx437", catatan: "pasangan terjauh biasanya di Kx210" },
+      { sebelumnya: "KBBKB", prediksi_pasangan: "Kx1 Kx2 Bx5 Bx11 Bx23 Kx49 Kx102 Kx210 Bx437", catatan: "pasangan terjauh biasanya di Kx210" },
+      { sebelumnya: "BKKBK", prediksi_pasangan: "Bx1 Bx2 Kx5 Kx11 Kx23 Bx49 Bx102 Bx210 Kx437", catatan: "pasangan terjauh biasanya di Bx210" },
+      { sebelumnya: "KBKKB", prediksi_pasangan: "Kx1 Kx2 Bx5 Bx11 Bx23 Kx49 Kx102 Kx210 Bx437", catatan: "pasangan terjauh biasanya di Kx102" },
+      { sebelumnya: "BKBBK", prediksi_pasangan: "Bx1 Bx2 Kx5 Kx11 Kx23 Bx49 Bx102 Bx210 Kx437", catatan: "pasangan terjauh biasanya di Bx102" },
+    ];
+    function cariData() {
+      var kataKunci = document.getElementById("kataKunci").value.toLowerCase();
+
+      var hasilPencarian = data.filter(function (item) {
+        return (
+          item.sebelumnya.toLowerCase() === kataKunci
+        );
+      });
+
+      var container = document.getElementById("hasilPencarian");
+      container.innerHTML = "";
+
+      hasilPencarian.forEach(function (item) {
+        var div = document.createElement("div");
+        var table = document.createElement("table");
+
+        var row1 = document.createElement("tr");
+        var th1 = document.createElement("th");
+        th1.innerText = "Sebelumnya";
+        var td1 = document.createElement("td");
+        td1.innerText = item.sebelumnya;
+        row1.appendChild(th1);
+        row1.appendChild(td1);
+
+        var row2 = document.createElement("tr");
+        var th2 = document.createElement("th");
+        th2.innerText = "Prediksi Pasangan";
+        var td2 = document.createElement("td");
+        td2.innerText = item.prediksi_pasangan;
+        row2.appendChild(th2);
+        row2.appendChild(td2);
+
+        var row3 = document.createElement("tr");
+        var th3 = document.createElement("th");
+        th3.innerText = "Catatan";
+        var td3 = document.createElement("td");
+        td3.innerText = item.catatan;
+        row3.appendChild(th3);
+        row3.appendChild(td3);
+
+        table.appendChild(row1);
+        table.appendChild(row2);
+        table.appendChild(row3);
+        div.appendChild(table);
+        container.appendChild(div);
+      });
+    }
